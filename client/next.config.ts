99b86c7edef1601dd7b2c925ea7ffa1abff2d074
Rohit Sharma <rohits8731@gmail.com>
2025-08-25 14:28:42 +0530

    Client Completed

--- client/next.config.ts
+++ client/next.config.ts
@@ -12,7 +12,7 @@ const nextConfig: NextConfig = {
         return [
             {
                 source: '/api/:path*',
-                destination: 'http://localhost:5000/api/:path*',
+                destination: `${process.env.NEXT_PUBLIC_BASE_URL}/api/:path*`,
             },
         ];
     },
--- client/src/utils/url.ts
+++ client/src/utils/url.ts
@@ -1,9 +1,9 @@
 export const generatePostbackUrl = (affiliateId: number) => {
-    const baseUrl = 'http://localhost:5000/postback'
+    const baseUrl = `${process.env.NEXT_PUBLIC_BASE_URL}/postback`
     return `${baseUrl}?affiliate_id=${affiliateId}&click_id={click_id}&amount={amount}&currency={currency}`
 }
 
 export const generateClickUrl = (affiliateId: number, campaignId: number) => {
-    const baseUrl = 'http://localhost:5000/click'
+    const baseUrl = `${process.env.NEXT_PUBLIC_BASE_URL}/click`
     return `${baseUrl}?affiliate_id=${affiliateId}&campaign_id=${campaignId}&click_id={click_id}`
 }
